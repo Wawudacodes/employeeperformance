2dcd1940ec056ffd23d5a7670516b38e59e3092b
wawudacodes <fwmwasi1@gmail.com>
2024-11-30 10:44:50 +0300

    Update INX_performance_prediction_app.py

--- INX_performance_prediction_app.py
+++ INX_performance_prediction_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 # Loading the trained model using joblib
 try:
-    loaded_model = joblib.load('C:\Users\User\Documents\IABAC\rf_trained_rfmodel.joblib')
+    loaded_model = joblib.load('C:/Users/User/Documents/IABAC/rf_trained_rfmodel.joblib')
 except Exception as e:
     loaded_model = None
     print(f"Error loading the model: {e}")
